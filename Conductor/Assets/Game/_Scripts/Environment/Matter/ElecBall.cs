0e0a41eafd0403dba9f3d08f9f800d2169cbdfd5
Reig219 <58218324+Reig219@users.noreply.github.com>
2020-01-23 10:18:44 -0500

    Sln file and Material System
    +Added Base Class for the Matter (Material) System
    +Added class for the projectile (ElecBall)

---
+++ Conductor/Assets/Game/_Scripts/Environment/Matter/ElecBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElecBall : BaseMatter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        onFire = true;
+        conducting = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
