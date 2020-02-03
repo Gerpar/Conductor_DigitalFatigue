7b310ff3a378687c0851b5b52a52b79764c7d093
Reig219 <58218324+Reig219@users.noreply.github.com>
2020-02-03 12:53:22 -0500

    Started Rope
    +Created a basic rope in my test scene

---
+++ Conductor/Assets/Game/_Scripts/Test/ConstMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstMove : MonoBehaviour
+{
+    public Vector3 Direction;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 speed = Direction * Time.deltaTime;
+        rb.MovePosition(transform.position + speed);
+    }
+}
