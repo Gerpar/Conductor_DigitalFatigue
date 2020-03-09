822415c70387714e23b4ad774bef0a3a06411be6
Gerpar <Gerpar.gerps@gmail.com>
2020-03-09 11:03:26 -0400

    Created basic settings UI Design, and implemented bloom slider

---
+++ Conductor/Assets/Game/_Scripts/UI/SettingsMenuUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SettingsMenuUI : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume processVolume;
+
+    Bloom bloom = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        processVolume.profile.TryGetSettings(out bloom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateBloom(float newBloomValue)
+    {
+        bloom.intensity.value = newBloomValue;
+    }
+}
